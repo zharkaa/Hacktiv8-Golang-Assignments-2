0f5ce8701fd2ebe854716819a75dad686bbc8690
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-09-30 16:52:06 +0800

    chore: fix error response structure

--- pkg/errs/err.go
+++ pkg/errs/err.go
@@ -9,8 +9,8 @@ type Error interface {
 }
 
 type ErrorData struct {
-	ErrMessage string `json:"message"`
 	ErrStatus  int    `json:"status"`
+	ErrMessage string `json:"message"`
 	ErrError   string `json:"error"`
 }
 
@@ -28,48 +28,48 @@ func (e *ErrorData) Error() string {
 
 func NewUnauthorizedError(message string) Error {
 	return &ErrorData{
-		ErrMessage: message,
 		ErrStatus:  http.StatusForbidden,
+		ErrMessage: message,
 		ErrError:   "NOT_AUTHORIZED",
 	}
 }
 
 func NewUnauthenticatedError(message string) Error {
 	return &ErrorData{
-		ErrMessage: message,
 		ErrStatus:  http.StatusUnauthorized,
+		ErrMessage: message,
 		ErrError:   "NOT_AUTHENTICATED",
 	}
 }
 
 func NewNotFoundError(message string) Error {
 	return &ErrorData{
-		ErrMessage: message,
 		ErrStatus:  http.StatusNotFound,
+		ErrMessage: message,
 		ErrError:   "NOT_FOUND",
 	}
 }
 
 func NewBadRequest(message string) Error {
 	return &ErrorData{
-		ErrMessage: message,
 		ErrStatus:  http.StatusBadRequest,
+		ErrMessage: message,
 		ErrError:   "BAD_REQUEST",
 	}
 }
 
 func NewInternalServerError(message string) Error {
 	return &ErrorData{
-		ErrMessage: message,
 		ErrStatus:  http.StatusInternalServerError, //500
+		ErrMessage: message,
 		ErrError:   "INTERNAL_SERVER_ERROR",
 	}
 }
 
 func NewUnprocessibleEntityError(message string) Error {
 	return &ErrorData{
-		ErrMessage: message,
 		ErrStatus:  http.StatusUnprocessableEntity,
+		ErrMessage: message,
 		ErrError:   "INVALID_REQUEST_BODY",
 	}
 }
